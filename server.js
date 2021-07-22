853da2805bdc511c0a39ec812a4f873ac3fe74a7
SmithRBG <smit36000@gmail.com>
2021-07-22 12:24:03 -0400

    server update

--- server.js
+++ server.js
@@ -2,22 +2,41 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
+const public = require('./Develop/public');
+
+app.use('/public', public); 
 
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => console.log (`Server listening on ${PORT}`));
 
-/* 
+ /* app.use(express.static(path.join(__dirname, './Develop/public'))); */
+
  app.get('/',(req, res) => {
     res.sendFile(path.join(__dirname,'./Develop/public/index.html'));
  });
 
- app.get('/',(req, res) => {
+ app.get('/notes',(req, res) => {
     res.sendFile(path.join(__dirname,'./Develop/public/notes.html'));
- }); */
+ }); 
 
- app.use(express.static(path.join(__dirname, './Develop/public')));
+ //route methods
+ app
+    .route('/api/notes')
+    .get((req, res) => {})
+    .post((req, res) => {});
 
+app
+ .route('*/api/notes/:id')
+    .get((req, res) => {})
+    .put((req, res) => {})
+    .delete((req, res) => {});
+
+ 
+
+
+
+ 
 /* app.use(noTaker); */
 
 /* const noTaker = (req, res, next) => {
